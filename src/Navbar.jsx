347a6b5f8248d33b853b5d86efe4f82703aff159
Vishal Shukla <vishal.shuklaji45@gmail.com>
2026-03-09 22:09:58 +0530

    Add animation in Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -32,38 +32,36 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`navbar navbar-expand-lg fixed-top transition-navbar ${
-        scrolled ? "navbar-glass" : "navbar-transparent"
-      }`}
-    >
+      className={`navbar navbar-expand-lg fixed-top transition-navbar ${scrolled ? "navbar-glass navbar-shrink" : "navbar-transparent navbar-normal"
+        }`}>
       <div className="container-fluid">
 
         <Link className="navbar-brand fw-bold text-light fs-4" to="/">
           VS
         </Link>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" style={{color:"white"}}>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto gap-lg-4">
             <li className="nav-item">
-              <NavLink to="/" className={navLinkStyle} onClick={closeNavbar} style={{color:"white"}}>
+              <NavLink to="/" className={navLinkStyle} onClick={closeNavbar} style={{ color: "white" }}>
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/about" className={navLinkStyle} onClick={closeNavbar} style={{color:"white"}}>
+              <NavLink to="/about" className={navLinkStyle} onClick={closeNavbar} style={{ color: "white" }}>
                 About
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/projects" className={navLinkStyle} onClick={closeNavbar} style={{color:"white"}}>
+              <NavLink to="/projects" className={navLinkStyle} onClick={closeNavbar} style={{ color: "white" }}>
                 Projects
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/resume" className={navLinkStyle} onClick={closeNavbar} style={{color:"white"}}>
+              <NavLink to="/resume" className={navLinkStyle} onClick={closeNavbar} style={{ color: "white" }}>
                 Resume
               </NavLink>
             </li>
